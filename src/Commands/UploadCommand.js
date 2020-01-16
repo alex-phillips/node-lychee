6ac9652509d789154c1d726070d08e5aeedd897e
alex-phillips <ahp118@gmail.com>
2020-01-16 08:17:46 -0500

    removed unused (old) command setup block in cli.js, fixed args passed in for files in upload command

--- src/Commands/UploadCommand.js
+++ src/Commands/UploadCommand.js
@@ -2,7 +2,7 @@ const BaseCommand = require('./BaseCommand');
 
 module.exports = class UploadCommand extends BaseCommand {
   async exec(cmd, args) {
-    for (const file of args.files) {
+    for (const file of args._.slice(1)) {
       const response = await this.api.upload(file, args.album || 0);
     }
   }
